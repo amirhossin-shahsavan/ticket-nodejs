24cfcbe6012148306eed4dc4fb9940764afd2e59
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-12-12 13:22:02 +0330

    complite ticket controller.

--- controller/ticketCtrl.js
+++ controller/ticketCtrl.js
@@ -1,11 +1,11 @@
 const Ticket = require("./../model/Ticket");
-
+const appErr = require("./../utils/errHandler");
 const getTicket = async (req, res) => {
   try {
     const tickets = await Ticket.findById(req.params.id);
     res.json(tickets);
   } catch (error) {
-    res.status(500).send(error);
+    next(appErr(error.message));
   }
 };
 const getallTicket = async (req, res) => {
@@ -13,17 +13,24 @@ const getallTicket = async (req, res) => {
     const tickets = await Ticket.find();
     res.json(tickets);
   } catch (error) {
-    res.status(500).send(error);
+    next(appErr(error.message));
   }
 };
 
 const createTicket = async (req, res) => {
+  const { title, description } = req.body;
   try {
-    const newTicket = new Ticket(req.body);
-    const savedTicket = await newTicket.save();
-    res.status(201).send(savedTicket);
+    const ticketCreated = await Ticket.create({
+      title,
+      description,
+    }).save();
+
+    res.json({
+      status: "success",
+      data: ticketCreated,
+    });
   } catch (error) {
-    res.status(500).send(error);
+    next(appErr(error.message));
   }
 };
 
@@ -34,26 +41,32 @@ const updateTicket = async (req, res) => {
       req.body,
       { new: true }
     );
-    if (updatedTicket) {
-      res.send(updatedTicket);
+    if (!updatedTicket) {
+      return next(appErr("Ticket not found"));
     } else {
-      res.status(404).send("Ticket not found");
+      res.json({
+        status: "success",
+        data: "you have successfully update ticket",
+      });
     }
   } catch (error) {
-    res.status(500).send(error);
+    next(appErr(error.message));
   }
 };
 
 const deleteTicket = async (req, res) => {
   try {
     const deletedTicket = await Ticket.findByIdAndDelete(req.params.id);
-    if (deletedTicket) {
-      res.status(204).send();
+    if (!deletedTicket) {
+      return next(appErr("Ticket not found"));
     } else {
-      res.status(404).send("Ticket not found");
+      res.json({
+        status: "success",
+        data: "you have successfully delete ticket",
+      });
     }
   } catch (error) {
-    res.status(500).send(error);
+    next(appErr(error.message));
   }
 };
 
@@ -64,3 +77,5 @@ module.exports = {
   updateTicket,
   deleteTicket,
 };
+
+
