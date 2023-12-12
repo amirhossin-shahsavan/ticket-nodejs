1cd89a2cedd39fceaf0b77c26b2dde7f40decb44
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-12-12 17:08:07 +0330

    create is login and is admin middleware.

---
+++ middleware/isAdmin.js
@@ -0,0 +1,28 @@
+const User = require("../model/User");
+const appErr = require("../utils/errHandler");
+const getTokenFromHeader = require("../utils/getTokenFromHeader");
+const verifyToken = require("../utils/verifyToken");
+
+const isAdmin = async (req, res, next) => {
+  const token = getTokenFromHeader(req);
+
+  const decodedUser = verifyToken(token);
+
+  req.userAuth = decodedUser.id;
+
+  const user = await User.findById(decodedUser.id);
+
+  if (user.isAdmin) {
+    return next();
+  } else {
+    next(appErr("access denied", 403));
+  }
+
+  if (!decodedUser) {
+    return next(appErr("invalid/Expire token , please login again"));
+  } else {
+    next();
+  }
+};
+
+module.exports = isAdmin;
---
+++ middleware/isLogin.js
@@ -0,0 +1,18 @@
+const appErr = require("../utils/errHandler");
+const getTokenFromHeader = require("../utils/getTokenFromHeader");
+const verifyToken = require("../utils/verifyToken");
+
+const isLogin = (req, res, next) => {
+  const token = getTokenFromHeader(req);
+
+  const decodedUser = verifyToken(token);
+
+  req.userAuth = decodedUser.id;
+  if (!decodedUser) {
+    return next(appErr("invalid/Expire token , please login again"));
+  } else {
+    next();
+  }
+};
+
+module.exports = isLogin;
